bd4a034164283e4fa767bf9890428d1049259701
KassandraSaint <kassandrastripper@gmail.com>
2022-06-17 17:32:20 -0400

    Add project files.

---
+++ CSVReader/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace CSVReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.averageBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.getBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // averageBox
+            // 
+            this.averageBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageBox.FormattingEnabled = true;
+            this.averageBox.ItemHeight = 36;
+            this.averageBox.Location = new System.Drawing.Point(107, 100);
+            this.averageBox.Name = "averageBox";
+            this.averageBox.Size = new System.Drawing.Size(582, 292);
+            this.averageBox.TabIndex = 1;
+            this.averageBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(598, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculate average of scores from a CSV file";
+            // 
+            // getBtn
+            // 
+            this.getBtn.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getBtn.Location = new System.Drawing.Point(107, 412);
+            this.getBtn.Name = "getBtn";
+            this.getBtn.Size = new System.Drawing.Size(270, 85);
+            this.getBtn.TabIndex = 2;
+            this.getBtn.Text = "Get Scores";
+            this.getBtn.UseVisualStyleBackColor = true;
+            this.getBtn.Click += new System.EventHandler(this.getBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(419, 412);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(270, 85);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.getBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitBtn;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.getBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.averageBox);
+            this.Name = "Form1";
+            this.Text = "CSV Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox averageBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button getBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ CSVReader/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSVReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getBtn_Click(object sender, EventArgs e)
+        {
+            // Trying to read a file
+            try
+            {
+                // Creating a StreamReader Object to read a file
+                StreamReader inputFile;
+
+                // Initializing variables to work with data
+                string line;
+                int count = 0;
+                int total;
+                double average;
+
+                // Creating an array for a delimiter
+                char[] delim = { ',' };
+
+                // Opening the file
+                inputFile = File.OpenText("Resources/Grades.csv");
+
+                // While there is data reading a file line by line
+                while (!inputFile.EndOfStream)
+                {
+                    // Incrementing student count
+                    count++;
+                    // Reading a line from the file into a line variable
+                    line = inputFile.ReadLine();
+
+                    // Separating tokens by delimkiter on each line
+                    string[] tokens = line.Split(delim);
+
+                    // Setting total to 0 
+                    total = 0;
+                    // Calculating total of each student by summing tokens of each line
+                    foreach (string str in tokens)
+                    {
+                        total += int.Parse(str);
+                    }
+
+                    // Calculating average of student's test scores
+                    average = (double)total / tokens.Length;
+
+                    // Displaying average of test scores in the Box List
+                    averageBox.Items.Add("The average for student " + count + " is " + average.ToString("n1"));
+                }
+                // Closing the file
+                inputFile.Close();
+            }
+            // Showing a message if the was an error
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // Closing the form
+            this.Close();
+        }
+    }
+}
